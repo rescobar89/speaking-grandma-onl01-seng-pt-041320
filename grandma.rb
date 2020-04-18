4c6c91289b5f92efae8fbc7f7920dd5783f02938
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-18 08:41:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     'I LOVE YOU TOO PUMPKIN!'
   elsif phrase != phrase.upcase
-    return 
+    return "HUH?! SPEAK UP, SONNY!"
+    
   end
 end
